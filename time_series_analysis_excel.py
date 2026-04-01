786317304c7d0c93478d0b60933be7e248f780bf
Bernardo Estevam <bernaz.10golden97@gmail.com>
2026-04-01 19:06:45 +0100

    typo 6 to 7 in length of df - forgot to change

--- time_series_analysis_excel.py
+++ time_series_analysis_excel.py
@@ -63,7 +63,7 @@ def load_excel_data(file_path, sheet_name, file_name):
         df = pd.read_excel(file_path, sheet_name=sheet_name, engine='calamine')
         
         if "filtered" in file_name.lower() and sheet_name == "Kinematics":
-            if len(df) > 6: 
+            if len(df) > 7: 
                 df = df.iloc[:-7]
                 
                 cols_to_convert = df.select_dtypes(include=['object']).columns
