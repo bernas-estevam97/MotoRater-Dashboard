3643f034d27a9f086a9adcc3afc86f6786b0ef59
Bernardo Estevam <bernaz.10golden97@gmail.com>
2026-05-04 16:01:13 +0100

    changed script organization and update license file

--- main.py
+++ main.py
@@ -5,70 +5,64 @@ from datetime import datetime
 st.set_page_config(page_title="MotoRater Suite", page_icon="📑", layout="wide")
 
 # 2. Define your pages
-# The file paths should point exactly to your script names
+# The file paths must now include your folder name so Streamlit can find them
 intro_page = st.Page(
-    page="intro.py", 
+    page="scripts/intro.py", 
     title="Welcome", 
     icon="🏠", 
     default=True  # This is now your launch homepage
 )
 
 tutorial_page_time = st.Page(
-    page="tutorial_time_series.py",
+    page="scripts/tutorial_time_series.py",
     title="Tutorial - Time Series",
     icon="❓",
 )
 
 tutorial_page_kine = st.Page(
-    page="tutorial_kinematics.py",
+    page="scripts/tutorial_kinematics.py",
     title="Tutorial - Kinematics Longitudinal Analyzer",
     icon="❓",
 )
 
 stat_info_page = st.Page(
-    page="stat_info.py",
+    page="scripts/stat_info.py",
     title="Statistical Methodology",
     icon="🧮"
 )
 
-
 all_measurements_page = st.Page(
-    page="time_series_analysis_excel.py", 
+    page="scripts/time_series_analysis_excel.py", 
     title="Time-Series analysis - Excel Files", 
     icon="📈",
 )
 
 all_measurements_parquet = st.Page(
-    page="time_series_analysis_parquet.py", 
+    page="scripts/time_series_analysis_parquet.py", 
     title="Time-Series analysis - Parquet files", 
     icon="📈",
 )
 
 kinematics_analysis_page = st.Page(
-    page="kinematic_analysis.py", 
+    page="scripts/kinematic_analysis.py", 
     title="Kinematics Longitudinal Analysis", 
     icon="📊" 
 )
 
 converter_page = st.Page(
-    page="convert_to_parquet.py", 
+    page="scripts/convert_to_parquet.py", 
     title="Excel -> Parquet Converter", 
     icon="⚡"
 )
 
 # 3. Create the navigation menu
-# Using a dictionary allows you to group pages under a nice header in the sidebar
 pg = st.navigation(
     {"Home": [intro_page, tutorial_page_time, tutorial_page_kine, stat_info_page],
      "MotoRater Tools": [all_measurements_page, all_measurements_parquet, kinematics_analysis_page, converter_page]}
 )
 
-
-
-
 # 4. Run the selected page
 pg.run()
-
 
 # --- NEW: Global Copyright in Sidebar ---
 st.sidebar.text("")
